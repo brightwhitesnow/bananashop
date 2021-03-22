4daf5c3acc892cc53bf0f5e1750701376e6f342a
brightwhitesnow <66269061+brightwhitesnow@users.noreply.github.com>
2021-03-22 10:44:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -115,8 +115,8 @@
                 <form id="contact" action="">
                     <input id="name" type="text" placeholder="Your Name" required>
                     <input id="message" type="text" placeholder="Message" required>
-                    <input id="phone" type="phone" placeholder="Your Phone Number" style="height: 50px;width: 70px;border-radius: 5px;">
-                    <input id="email" type="email" placeholder="Your Email" required style="height: 50px;width: 70px;border-radius: 5px;">
+                    <input id="phone" type="phone" placeholder="Your Phone Number" style="height: 45px;width: 500px;border-radius: 5px;"><br>
+                    <input id="email" type="email" placeholder="Your Email" required style="height: 45px;width: 500px;border-radius: 5px;"><br>
                     <button>Submit</button>
                     
 
